8ea7ae80018eeefb7c968273677c95f093570c66
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2018-12-04 17:45:09 +0200

    week1-002

--- week1-002.HelloWorld/src/Greeter.java
+++ week1-002.HelloWorld/src/Greeter.java
@@ -2,7 +2,10 @@ public class Greeter {
 
     public static void main(String[] args) {
         // Write your code here
-        System.out.println("test");
+        System.out.print("Hello ");
+        System.out.print("world");
+        System.out.print("!");
+        System.out.print("\n(And all the people of the world)\n");
 
     }
 
