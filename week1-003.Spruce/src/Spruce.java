fc84a1321db63d813b77a6cbdfb460117b3314ee
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2018-12-04 17:56:25 +0200

    week1-003

--- week1-003.Spruce/src/Spruce.java
+++ week1-003.Spruce/src/Spruce.java
@@ -3,7 +3,8 @@ public class Spruce {
     public static void main(String[] args) {
 
         // Write code here
-        System.out.println("*");
+        System.out.println("    *\n   ***\n  *****\n *******\n*********\n    *");
+
     }
 
 }
