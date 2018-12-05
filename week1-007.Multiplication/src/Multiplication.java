5d9550573a7e4afa1528f1ff06c15c379ccbf632
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2018-12-05 09:46:21 +0200

    week1-007

--- week1-007.Multiplication/src/Multiplication.java
+++ week1-007.Multiplication/src/Multiplication.java
@@ -4,8 +4,10 @@ public class Multiplication {
 
         int a = 1337;
         int b = 42;
+        int result = (a*b);
 
-        // Program your solution here. Remember to use variables a and b!
+        String toPrint = a + " * " + b + " = " + result;
+        System.out.println(toPrint);// Program your solution here. Remember to use variables a and b!
     }
 
 }
