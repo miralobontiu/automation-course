abecb9892995c1981387ae80d0d1b67f6ddf1795
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2018-12-06 18:03:28 +0200

    week1-010

--- week1-010.CircleCircumference/src/Circumference.java
+++ week1-010.CircleCircumference/src/Circumference.java
@@ -5,6 +5,11 @@ public class Circumference {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        // Program your solution here 
+        System.out.println("Type the radius: ");
+        int a = Integer.parseInt(reader.nextLine());
+
+        double pi = Math.PI;
+        double result = (double)2*pi*a;
+        System.out.println("Circumference of the circle: " + result);// Program your solution here
     }
 }
