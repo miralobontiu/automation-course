9344c2b5754ad717ffaece7d4dea8114cfef5b97
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2018-12-06 17:42:00 +0200

    week1-009

--- week1-009.Divider/src/Divider.java
+++ week1-009.Divider/src/Divider.java
@@ -6,6 +6,15 @@ public class Divider {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.println("Type a number: ");
+        int a = Integer.parseInt(reader.nextLine());
+        System.out.println("Type another number: ");
+        int b = Integer.parseInt(reader.nextLine());
+
+        double quotient = (double)a/b;
+        String toPrint = a + " / " + b + " = " + quotient;
+        System.out.println("Division: " + toPrint);
+
         // Implement your program here. Remember to ask the input from user.
     }
 }
