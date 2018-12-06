b249b8180bdbf4762b55d98f2f65f13fad7bfa05
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2018-12-06 11:08:39 +0200

    week1-008

--- week1-008.Adder/src/Adder.java
+++ week1-008.Adder/src/Adder.java
@@ -6,6 +6,14 @@ public class Adder {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        // Implement your program here. Remember to ask the input from user
+        System.out.println("Type a number: ");
+        int a = Integer.parseInt(reader.nextLine());
+
+        System.out.println("Type another number: ");
+        int b = Integer.parseInt(reader.nextLine());
+        int sum = (a + b);
+        System.out.println("Sum of the numbers: " + sum);// Implement your program here. Remember to ask the input from user
     }
+
+
 }
