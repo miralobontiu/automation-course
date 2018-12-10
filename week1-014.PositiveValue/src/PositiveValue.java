dc4621fc8cc0d838de4d8922d90a5d80d04a7993
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2018-12-10 10:19:44 +0200

    week1-014

--- week1-014.PositiveValue/src/PositiveValue.java
+++ week1-014.PositiveValue/src/PositiveValue.java
@@ -7,5 +7,12 @@ public class PositiveValue {
         Scanner reader = new Scanner(System.in);
 
         // Type your program here:
+        System.out.println("Type a number: ");
+        int a = Integer.parseInt(reader.nextLine());
+        if (a > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is not positive.");
+        }
     }
 }
