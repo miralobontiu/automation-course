12bf33e98152459a46329ba281374fe76ef15c26
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2018-12-10 13:03:48 +0200

    week1-020

--- week1-020.Usernames/src/Usernames.java
+++ week1-020.Usernames/src/Usernames.java
@@ -6,5 +6,26 @@ public class Usernames {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.println("Type your username: ");
+        String userName = reader.nextLine();
+        System.out.println("Type your password: ");
+        String password = reader.nextLine();
+
+        if (userName.equals("alex") && password.equals("mightyducks")) {
+            System.out.println("You are now logged into the system!");
+
+        } else if (userName.equals("emily") && password.equals("cat")) {
+            System.out.println("You are now logged into the system!");
+
+        } else if (!(userName.equals("alex")) || !(password.equals("mightyducks"))) {
+            System.out.println("Your username or password was invalid!");
+
+        } else if (!(userName.equals("emily")) || !(password.equals("cat"))) {
+            System.out.println("Your username or password was invalid!");
+        } else {
+            System.out.println("Error!");
+        }
     }
+
 }
+
