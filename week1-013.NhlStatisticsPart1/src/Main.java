ab191b3981d25efe1af3dca0c2227ffd8f4096a2
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2018-12-10 10:07:26 +0200

    week1-013

--- week1-013.NhlStatisticsPart1/src/Main.java
+++ week1-013.NhlStatisticsPart1/src/Main.java
@@ -1,8 +1,31 @@
 
+
 import nhlstats.NHLStatistics;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        //System.out.println("Top ten by points");
+        //NHLStatistics.sortByPoints();
+        //NHLStatistics.top(10);
+
+        System.out.println("Top ten by goals");
+        NHLStatistics.sortByGoals();
+        NHLStatistics.top(10);
+
+        System.out.println("Top 25 by penalties");
+        NHLStatistics.sortByPenalties();
+        NHLStatistics.top(25);
+
+        System.out.println("Statistics for Sidney Crosby:");
+        NHLStatistics.searchByPlayer("Sidney Crosby");
+
+        System.out.println("Team statistics for Philadelphia Flyers:");
+        NHLStatistics.teamStatistics("PHI");
+        NHLStatistics.sortByPoints();
+
+        System.out.println("The players in Anaheim Ducks:");
+        NHLStatistics.teamStatistics("ANA");
     }
 }
