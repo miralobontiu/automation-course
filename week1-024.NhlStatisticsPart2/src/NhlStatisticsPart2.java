36c719f694e9df652ec0b7e9e4c5827d4bc8e9f3
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2018-12-11 13:02:03 +0200

    week1-024+

--- week1-024.NhlStatisticsPart2/src/NhlStatisticsPart2.java
+++ week1-024.NhlStatisticsPart2/src/NhlStatisticsPart2.java
@@ -1,5 +1,6 @@
 
 import java.util.Scanner;
+
 import nhlstats.NHLStatistics;
 
 public class NhlStatisticsPart2 {
@@ -10,7 +11,6 @@ public class NhlStatisticsPart2 {
         System.out.println("NHL statistics service");
 
         while (true) {
-            System.out.println("");
             System.out.print("command (points, goals, assists, penalties, player, club, quit): ");
             String command = reader.nextLine();
 
@@ -19,16 +19,27 @@ public class NhlStatisticsPart2 {
             }
 
             if (command.equals("points")) {
-                // Print the top ten players sorted by points.
+                NHLStatistics.sortByPoints();
+                NHLStatistics.top(10);// Print the top ten players sorted by points.
             } else if (command.equals("goals")) {
-                // Print the top ten players sorted by goals.
+                NHLStatistics.sortByGoals();
+                NHLStatistics.top(10);// Print the top ten players sorted by goals.
             } else if (command.equals("assists")) {
-                //  Print the top ten players sorted by assists.
+                NHLStatistics.sortByAssists();
+                NHLStatistics.top(10);//  Print the top ten players sorted by assists.
             } else if (command.equals("penalties")) {
-                //  Print the top ten players sorted by penalties.
+                NHLStatistics.sortByPenalties();
+                NHLStatistics.top(10);//  Print the top ten players sorted by penalties.
             } else if (command.equals("player")) {
+                String playerName = reader.nextLine();
+                NHLStatistics.searchByPlayer(playerName);
                 // Ask the user first which player's statistics are needed and then print them.
             } else if (command.equals("club")) {
+                String clubName = reader.nextLine();
+                NHLStatistics.sortByPoints();
+                NHLStatistics.teamStatistics(clubName);
+//                NHLStatistics.teamStatistics();
+//                NHLStatistics.top(10);
                 // Ask the user first which club's statistics are needed and then print them.
                 // Note: When printing statistics they should be ordered by points (so the players with the most points come first).
             }
