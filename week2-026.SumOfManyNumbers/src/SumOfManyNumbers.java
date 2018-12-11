e022cabf88d808759aa1f80893e59654ce28383e
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2018-12-11 15:55:22 +0200

    week2-026

--- week2-026.SumOfManyNumbers/src/SumOfManyNumbers.java
+++ week2-026.SumOfManyNumbers/src/SumOfManyNumbers.java
@@ -7,18 +7,16 @@ public class SumOfManyNumbers {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         int sum = 0;
-        
+
         while (true) {
+            System.out.println("Type a number: ");
             int read = Integer.parseInt(reader.nextLine());
+            sum = sum + read;
             if (read == 0) {
                 break;
             }
-
-            // do something here
-
-            System.out.println("Sum now: " + sum);
+                System.out.println("Sum now: " + sum);
+            }
+            System.out.println("Sum in the end: " + sum);
         }
-        
-        System.out.println("Sum in the end: " + sum);
     }
-}
