d4e7b7c1650abdfb0f153489284ed887746f493c
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2018-12-11 12:12:20 +0200

    week1-023

--- week1-023.Temperatures/src/Temperatures.java
+++ week1-023.Temperatures/src/Temperatures.java
@@ -6,14 +6,26 @@ public class Temperatures {
     public static void main(String[] args) {
 
         Scanner reader = new Scanner(System.in);
-        // Write your code here. 
 
-        // Graph is used as follows:
-        Graph.addNumber(7);
-        double value = 13.5;
-        Graph.addNumber(value);
-        value = 3;
-        Graph.addNumber(value);
-        // Remove or comment out these lines above before trying to run the tests.
+
+        while (true) {
+            System.out.println("Type the today`s temperature at 12.oo am: ");
+            double temperature = Double.parseDouble(reader.nextLine());
+            if (temperature < -30 || temperature > 40) {
+                System.out.println("Type a value between -30 and +40 degrees.");
+
+            } else {
+                Graph.addNumber(temperature);
+            }
+        }
     }
 }
+        // Graph is used as follows:
+        //Graph.addNumber(13.0);
+        //double value = 13.5;
+        //Graph.addNumber(value);
+        //value = 3;
+        //Graph.addNumber(value);
+        // Remove or comment out these lines above before trying to run the tests.
+
+
