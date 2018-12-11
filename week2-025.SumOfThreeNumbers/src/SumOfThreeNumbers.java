9467499ff6dde119db306abed9bd9323aab7559b
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2018-12-11 15:35:43 +0200

    week2-025

--- week2-025.SumOfThreeNumbers/src/SumOfThreeNumbers.java
+++ week2-025.SumOfThreeNumbers/src/SumOfThreeNumbers.java
@@ -5,14 +5,20 @@ public class SumOfThreeNumbers {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        int sum = 0;
-        int read; // store numbers read form user in this variable
+
+        // store numbers read form user in this variable
+        System.out.println("Type the first number: ");
+        int a = Integer.parseInt(reader.nextLine());
+        System.out.println("Type the second number: ");
+        int b = Integer.parseInt(reader.nextLine());
+        System.out.println("Type the third number: ");
+        int c = Integer.parseInt(reader.nextLine());
+        int read = a+b+c;
+        int sum = read;
 
 
-        // Write your program here
-        // Use only variables sum and read
-
-        sum = read;
+        //System.out.println(sum);
+        //sum = read;
 
         System.out.println("Sum: " + sum);
     }
