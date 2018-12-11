3c1d4dda4dceb0b1e8f677b259c72c6c3346fe8a
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2018-12-11 16:10:08 +0200

    week2-027

--- week2-027.FromOneToHundred/src/FromOneToHundred.java
+++ week2-027.FromOneToHundred/src/FromOneToHundred.java
@@ -2,6 +2,13 @@
 public class FromOneToHundred {
 
     public static void main(String[] args) {
-        // Write your program here
+
+        int number = 1;
+
+        while (number < 101) {
+            System.out.println(number);
+            number++;
+            // Write your program here
+        }
     }
 }
