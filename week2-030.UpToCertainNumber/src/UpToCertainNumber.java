00e6e07984127c5cd5dd7e0fa634e5bc6b870f1c
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2018-12-11 16:46:55 +0200

    week2-030

--- week2-030.UpToCertainNumber/src/UpToCertainNumber.java
+++ week2-030.UpToCertainNumber/src/UpToCertainNumber.java
@@ -6,7 +6,14 @@ public class UpToCertainNumber {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        
+        int number = 1;
+        System.out.println("Up to what number?" );
+        int limit = Integer.parseInt(reader.nextLine());
+
+        while (number <= limit) {
+            System.out.println(number);
+            number++;
+        }
         // Write your code here
         
     }
