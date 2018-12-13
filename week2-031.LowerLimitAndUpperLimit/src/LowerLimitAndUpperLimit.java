901003f4e527eb2fd3c8d08d55d895d4e4392a9c
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2018-12-13 09:43:44 +0200

    week2-031

--- week2-031.LowerLimitAndUpperLimit/src/LowerLimitAndUpperLimit.java
+++ week2-031.LowerLimitAndUpperLimit/src/LowerLimitAndUpperLimit.java
@@ -6,7 +6,18 @@ public class LowerLimitAndUpperLimit {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        // write your code here
+        System.out.println("First: ");
+        int a = Integer.parseInt(reader.nextLine());
+        System.out.println("Last: ");
+        int b = Integer.parseInt(reader.nextLine());
+
+        if (a > b) {
+        } else {
+            while (a <= b) {
+                System.out.println(a);
+                a++;
+            }
+        }
 
     }
 }
