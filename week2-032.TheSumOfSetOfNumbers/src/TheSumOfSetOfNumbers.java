75e321b21ef43735cd21567cddfa96b72ba03ace
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2018-12-18 14:32:24 +0200

    week2-032

--- week2-032.TheSumOfSetOfNumbers/src/TheSumOfSetOfNumbers.java
+++ week2-032.TheSumOfSetOfNumbers/src/TheSumOfSetOfNumbers.java
@@ -6,5 +6,17 @@ public class TheSumOfSetOfNumbers {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.print("Until what? ");
+        int n = Integer.parseInt(reader.nextLine());
+
+        int i = 0;
+        int sum = 0;
+
+        while(i <= n) {
+            sum += i;
+            i++;
+        }
+
+        System.out.println("Sum is " + sum + ".");
     }
 }
