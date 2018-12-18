1582083fb66360f6b6e5cb9ee658a350b3eca551
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2018-12-18 16:42:13 +0200

    week2-034

--- week2-034.Factorial/src/Factorial.java
+++ week2-034.Factorial/src/Factorial.java
@@ -4,5 +4,16 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.print("Type a number: ");
+        int a = Integer.parseInt(reader.nextLine());
+
+        int factorial = 1;
+        int number = 1;
+
+        while(number <= a) {
+            factorial *= number;
+            number++;
+        }
+        System.out.println("Factorial is " + factorial);
     }
 }
