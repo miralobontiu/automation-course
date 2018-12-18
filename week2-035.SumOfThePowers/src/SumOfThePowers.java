d41494fda9e6ab53e8c324f4d6bdf98c76bb3c9f
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2018-12-18 17:19:03 +0200

    week2-035

--- week2-035.SumOfThePowers/src/SumOfThePowers.java
+++ week2-035.SumOfThePowers/src/SumOfThePowers.java
@@ -5,6 +5,17 @@ public class SumOfThePowers {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        System.out.print("Type a number: ");
+        int power = Integer.parseInt(reader.nextLine());
 
+        int i = 0;
+        int result = 0;
+
+        while (i <= power) {
+            result += Math.pow(2, i);
+            i++;
+        }
+
+        System.out.println("The result is " + result);
     }
 }
