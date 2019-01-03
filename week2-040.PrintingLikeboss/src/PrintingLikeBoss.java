77bca906b1d660a324a3dbb45b5a5dca6056a0e7
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2019-01-03 14:12:34 +0200

    week2-040

--- week2-040.PrintingLikeboss/src/PrintingLikeBoss.java
+++ week2-040.PrintingLikeboss/src/PrintingLikeBoss.java
@@ -2,18 +2,43 @@ public class PrintingLikeBoss {
 
     // copy or rewrite the method of Assignment 39.1 here
     public static void printStars(int amount) {
+        int i = 0;
+        while (i < amount) {
+            System.out.print("*");
+            i++;
+        }
+        System.out.println();
     }
 
     public static void printWhitespaces(int amount) {
-        // 40.1
+        int i = 0;
+        while (i < amount) {
+            System.out.print(" ");
+            i++;
+        }
     }
 
     public static void printTriangle(int size) {
-        // 40.2
+        int i = 1;
+        while (i <= size) {
+            printWhitespaces(size - i);
+            printStars(i);
+            i++;
+        }
     }
 
     public static void xmasTree(int height) {
-        // 40.3
+        int i = 1;
+        while (i <= height) {
+            printWhitespaces(height - i);
+            printStars(2 * i - 1);
+            i++;
+        }
+        i = 2;
+        while (--i >= 0) {
+            printWhitespaces(height - 2);
+            printStars(3);
+        }
     }
 
     public static void main(String[] args) {
