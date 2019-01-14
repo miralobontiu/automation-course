744f98328c28769d0c966d3b76e00f98de6342b7
unknown <mirabela.lobontiu@P6718.ro.softvision.global>
2019-01-14 14:37:48 +0200

    week2-041

--- week2-041.GuessingNumberGame/src/GuessingNumberGame.java
+++ week2-041.GuessingNumberGame/src/GuessingNumberGame.java
@@ -8,10 +8,25 @@ public class GuessingNumberGame {
         Scanner reader = new Scanner(System.in);
         int numberDrawn = drawNumber();
 
-        // program your solution here. Do not touch the above lines!
-        
-    }
+        int score = 0;
+        int a = 0;
 
+        while (true) {
+            System.out.println("Guess a number: ");
+            a = reader.nextInt();
+
+            if (a == numberDrawn) {
+                System.out.println("Congratulations, your guess is correct!");
+                break;
+            } else if (a < numberDrawn) {
+                score++;
+                System.out.println("The number is greater, guesses made: " + score);
+            } else {
+                score++;
+                System.out.println("The number is lesser, guesses made: " + score);
+            }
+        }
+    }
     // DO NOT MODIFY THIS!
     private static int drawNumber() {
         return new Random().nextInt(101);
